607b7bc9fc69ce6426883316b1681d84f16e40fa
Arthur-Prince <r2.arthur.prince@gmail.com>
2025-11-29 18:43:59 -0300

    ajustando formatação so que agora eu verifiquei

--- src/adapters/external/models/LineInfo.py
+++ src/adapters/external/models/LineInfo.py
@@ -2,10 +2,10 @@ from typing import TypedDict
 
 
 class LineInfo(TypedDict):
-    cl: int     # código da linha (código interno SPTrans)
-    lc: bool    # sentido preferencial (circular / comum)
-    lt: str     # número da linha (ex: "8000")
-    sl: int     # sentido (0: ida, 1: volta)
-    tl: int     # tipo da linha (10 = urbana, etc.)
-    tp: str     # ponto inicial
-    ts: str     # ponto final
+    cl: int  # código da linha (código interno SPTrans)
+    lc: bool  # sentido preferencial (circular / comum)
+    lt: str  # número da linha (ex: "8000")
+    sl: int  # sentido (0: ida, 1: volta)
+    tl: int  # tipo da linha (10 = urbana, etc.)
+    tp: str  # ponto inicial
+    ts: str  # ponto final
